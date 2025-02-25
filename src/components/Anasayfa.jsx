6eb7aefe69603e726248ae6a71521af64d3ea3d6
Eren Gündüz <erengndz57@outlook.com>
2025-02-25 13:58:43 +0300

    Sipariş sayfası html eklendi

--- src/components/Anasayfa.jsx
+++ src/components/Anasayfa.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import homeBanner from '/images/iteration-1-images/home-banner.png';
 import homeLogo from '/images/iteration-1-images/logo.svg';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
 
 export default function Anasayfa() {
+  const history = useHistory();
+
+  const handleClick = () => {
+    history.push('/siparis')
+  }
   return (
     <div className="header" style={{ backgroundImage: `url(${homeBanner})` }}>
       <div className="header-content">
@@ -10,7 +16,7 @@ export default function Anasayfa() {
         <p className="anasayfa-text">
           KOD ACIKTIRIR <br /> PİZZA, DOYURUR
         </p>
-        <button className="header-button">Acıktım</button>
+        <button onClick={handleClick} className="header-button">Acıktım</button>
       </div>
     </div>
   );
--- src/components/Siparis.jsx
+++ src/components/Siparis.jsx
@@ -1,11 +1,39 @@
 import React, { useEffect, useState } from 'react';
 import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import logo from "/images/iteration-1-images/logo.svg";
 
 
 export default function () {
   return (
     <>
+      <header className="form-header">
+        <img src={logo} />
+      </header>
 
+      <section className='siparis-alani-container'>
+        <div className='siparis-alani-content'>
+          <nav className="nav-menu">
+            <a href="/">Anasayfa </a>
+            <p> - </p>
+            <a href="/siparis-olustur"> Sipariş Oluştur</a>
+            <h2>Position Absolute Acı Pizza</h2>
+            <div className="pizza-bilgi">
+          <h1>85.5 ₺</h1>
+          <p>4.9</p>
+          <p>(200)</p>
+        </div>
+        <p style={{ color: "#5F5F5F" }}>
+          Frontend Dev olarak hala position:absolute kullanıyorsan bu çok acı
+          pizza tam sana göre. Pizza, domates, peynir ve genellikle çeşitli
+          diğer malzemelerle kaplanmış, daha sonra geleneksel olarak odun
+          ateşinde bir fırında yüksek sıcaklıkta pişirilen, genellikle yuvarlak,
+          düzleştirilmiş mayalı buğday bazlı hamurdan oluşan İtalyan kökenli
+          lezzetli bir yemektir. Küçük bir pizzaya bazen pizzetta denir.
+        </p>
+          </nav>
+        </div>
+
+      </section>
     </>
   )
 }
